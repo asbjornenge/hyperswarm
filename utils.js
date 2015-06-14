153f0b1a07127770d0fa67b2e4c7fd55126417ef
Asbjørn Enge <asbjorn@hanafjedle.net>
2015-06-14 23:38:19 +0200

    Binding to, and more importantly brodcasting availability on, active interface

--- utils.js
+++ utils.js
@@ -1,9 +1,20 @@
 var freeport = require('freeport')
+var network  = require('network')
 
 var utils = {
     initNode : function(node, cb) {
+        var finish = function() {
+            if (node.options.host && node.options.port) cb()
+        }
         node.options.id   = node.id
-        node.options.host = node.options.host || '0.0.0.0'
+        node.options.host = node.options.host || network.get_active_interface(function(err, iface) {
+            if (err) { 
+                console.error('Error getting network interfaces', err); 
+                process.exit(1) 
+            }
+            node.options.host = iface.ip_address 
+            finish()
+        }) 
         node.options.port = node.options.port || freeport(function(err, port) {
             if (err) { 
                 console.error('Error getting random port', err); 
@@ -11,7 +22,7 @@ var utils = {
             }
             node.options.port = port
             node.ready = true
-            cb()
+            finish()
         })
         if (node.options.port) { node.ready = true; cb() }
         return node
